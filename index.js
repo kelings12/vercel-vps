b279e732a02e560cbe898ee91df3f05515563526
kelings12 <kelings12@icloud.com>
2026-08-31 13:37:52 +0800

    Update index.js
    dd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || 'd1cf4b9c-3e57-085d-b34a-797fcf601381';
+const UUID = process.env.UUID || '279e6161-1922-48b6-aab3-ac13a592e3e5';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
